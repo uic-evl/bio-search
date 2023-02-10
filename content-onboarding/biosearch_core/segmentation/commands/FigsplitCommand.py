78911a93c80818693f688736d434b9ea8cbe12c5
Juan Trelles <juantrelles@gmail.com>
2023-02-09 18:22:33 -0600

    change run to popen

--- content-onboarding/biosearch_core/segmentation/commands/FigsplitCommand.py
+++ content-onboarding/biosearch_core/segmentation/commands/FigsplitCommand.py
@@ -8,13 +8,17 @@ class FigsplitDockerCommand:
     """Execute figsplit on docker"""
 
     def __init__(self, instance_name):
-        self.command = f"docker exec ${instance_name} sh figsplit.sh "
+        self.command = f"docker exec {instance_name} sh figsplit.sh "
 
     def execute(self, doc_folder: str):
         """Iterate over folder and segment images"""
         pathlist = Path(doc_folder).rglob("*.jpg")
         for img_path in pathlist:
-            output = subprocess.run(
-                [self.command, str(img_path)], capture_output=True, check=True
-            )
-            print(output)
+            cmd = f"{self.command} {str(img_path)}".split()
+            # output = subprocess.run(
+            #     [self.command, str(img_path)], capture_output=True, check=True
+            # )
+            p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+            out, err = p.communicate()
+            print("out", out)
+            print("err", err)
