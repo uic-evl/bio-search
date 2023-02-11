d920a2f89f46836f0ecd7e39e524fca17b9f502a
Juan Trelles <juantrelles@gmail.com>
2023-02-11 15:12:41 -0600

    fix local and docker paths

--- content-onboarding/biosearch_core/segmentation/commands/FigsplitCommand.py
+++ content-onboarding/biosearch_core/segmentation/commands/FigsplitCommand.py
@@ -12,11 +12,17 @@ class FigsplitDockerCommand:
         self.command = f"docker exec {instance_name} sh figsplit.sh "
 
     def execute(self, doc_folder: str):
-        """Iterate over folder and segment images"""
+        """Iterate over folder and segment images
+        Parameters:
+        doc_folder: str
+            Location of the document with figures to extract on local machine
+        """
         pathlist = [elem for elem in listdir(Path(doc_folder)) if elem.endswith(".jpg")]
         print(pathlist)
-        for img_path in pathlist:
-            cmd = f"{self.command} {str(img_path)}".split()
+        folder_name = Path(doc_folder).name
+        for img_name in pathlist:
+            img_docker_path = Path("/mnt") / folder_name / img_name
+            cmd = f"{self.command} {str(img_docker_path)}".split()
             # output = subprocess.run(
             #     [self.command, str(img_path)], capture_output=True, check=True
             # )
