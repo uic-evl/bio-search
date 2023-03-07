9699260b94ab27041825a6e5bca8c068fbbf91a3
Juan Trelles <juantrelles@gmail.com>
2023-03-07 17:09:20 -0600

    upgrade nx and storybook

--- nx/search-publications-and-figures/libs/annotations/.storybook/main.ts
+++ nx/search-publications-and-figures/libs/annotations/.storybook/main.ts
@@ -1,22 +1,20 @@
-import type {StorybookConfig} from '@storybook/core-common'
-
-const config: StorybookConfig = {
-  core: {builder: 'webpack5'},
-  stories: [
-    '../src/lib/**/*.stories.mdx',
-    '../src/lib/**/*.stories.@(js|jsx|ts|tsx)',
-  ],
-  addons: [
-    '@storybook/addon-essentials',
-    '@nrwl/react/plugins/storybook',
-    '@chakra-ui/storybook-addon',
-  ],
+import type { StorybookConfig } from '@storybook/core-common';
+const config: StorybookConfig = ({
+  core: {},
+  stories: ['../src/lib/**/*.mdx', '../src/lib/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-essentials', '@nrwl/react/plugins/storybook', '@chakra-ui/storybook-addon', '@storybook/addon-mdx-gfm'],
   features: {
-    emotionAlias: false,
+    emotionAlias: false
   },
-} as StorybookConfig
-
-module.exports = config
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
+} as StorybookConfig);
+module.exports = config;
 
 // To customize your webpack configuration you can use the webpackFinal field.
 // Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
