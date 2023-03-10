12d0d097fbe8c1db1b2cf80ce9a40b2b2d1ad916
Juan Trelles <juantrelles@gmail.com>
2023-03-10 14:52:26 -0600

    predict probs

--- image-modalities-classifier/image_modalities_classifier/models/predict.py
+++ image-modalities-classifier/image_modalities_classifier/models/predict.py
@@ -8,6 +8,7 @@ from torch.cuda import empty_cache
 # pylint: disable=no-name-in-module
 from torch import no_grad, max as torch_max
 from torch.utils.data import DataLoader
+import torch.nn.functional as nnf
 from numpy import ndarray, hstack, vstack
 from pandas import DataFrame
 from sklearn.preprocessing import LabelEncoder
@@ -71,6 +72,27 @@ class SingleModalityPredictor:
             prediction_stack = hstack(predictions)
             return prediction_stack
 
+    def _predict_with_probs(self, dataloader: DataLoader) -> ndarray:
+        """Predict over dataloader and return a list of classes"""
+        self.model.eval()
+        probabilities = []
+        predictions = []
+        with no_grad():
+            for batch_imgs in dataloader:
+                data = batch_imgs.to(self.config.device)
+                batch_outputs = self.model(data)
+
+                probs = nnf.softmax(batch_outputs, dim=1)
+                _, batch_predictions = torch_max(batch_outputs, dim=1)
+
+                probs = probs.cpu().tolist()
+                batch_predictions = batch_predictions.cpu().tolist()
+
+                predictions += batch_predictions
+                probabilities += probs
+
+            return predictions, probabilities
+
     def _as_classes(self, predictions) -> List[str]:
         return self.decoder.inverse_transform(predictions)
 
@@ -88,6 +110,21 @@ class SingleModalityPredictor:
         if as_classes:
             return self._as_classes(predictions)
         return predictions
+
+    def predict_with_probs(
+        self,
+        data: DataFrame,
+        base_img_dir: str,
+        path_col: str = "img_path",
+        as_classes: bool = True,
+    ) -> List[str]:
+        """Predict from input dataframe and also return prediction probabilities"""
+        loader = self._get_dataloader(data, base_img_dir, path_col=path_col)
+        self.model.to(self.config.device)
+        predictions, probabilities = self._predict_with_probs(loader)
+        if as_classes:
+            return self._as_classes(predictions), probabilities
+        return predictions, probabilities
 
     def features(self, data: DataFrame, base_img_dir: str, path_col: str = "img_path"):
         """Extract features from layer before FC"""
