d05556dd0bf8ed1bacaf2dd0be5b0d739c2f09e6
Juan Trelles <juantrelles@gmail.com>
2023-07-28 16:39:13 -0500

    console script

--- search-engine/src/index.py
+++ search-engine/src/index.py
@@ -18,48 +18,49 @@ from CordReader import CordReader
 
 console = Console()
 
+
 def parse_args(args) -> Namespace:
     """Parse args from command line"""
     parser = ArgumentParser(prog="export indexes to parquet")
     parser.add_argument("input_path", type=str, help="path to parquet file to index")
     parser.add_argument("output_path", type=str, help="path to index storage")
-    parser.add_argument('-c', '--cord19_base_path', type=str, default="")
+    parser.add_argument("-c", "--cord19_base_path", type=str, default="")
     parsed_args = parser.parse_args(args)
 
     return parsed_args
 
+
 def main():
     """entry point"""
+    vm_env = lucene.getVMEnv() or lucene.initVM(vmargs=["-Djava.awt.headless=true"])
+    vm_env.attachCurrentThread()
     args = parse_args(sys.argv[1:])
 
     with console.status("[bold green] indexing data..."):
-      try:      
-          fulltext_provider = None
-          if args.cord19_base_path != "":
-              console.log("Found text provider")
-              fulltext_provider = CordReader(args.cord19_base_path)
+        try:
+            fulltext_provider = None
+            if args.cord19_base_path != "":
+                console.log("Found text provider")
+                fulltext_provider = CordReader(args.cord19_base_path)
 
-          console.log("Reading parquet")
-          dataframe = read_parquet(args.input_path, engine="pyarrow")
-          dataframe.reset_index()
-          console.log(f"Indexing {dataframe.shape[0]} documents")
+            console.log("Reading parquet")
+            dataframe = read_parquet(args.input_path, engine="pyarrow")
+            dataframe.reset_index()
+            console.log(f"Indexing {dataframe.shape[0]} documents")
 
-          start_time = time.time()
-          indexer = Indexer(args.output_path, create_mode=True)
-          indexer.index_from_dataframe(dataframe, fulltext_provider, split_term=";")
-          end_time = time.time()
-          console.log(f"[bold green] Finished after {end_time - start_time}")
-          sys.exit(os.EX_OK)
-      except FileNotFoundError:
-          console.log("[bold red] Input parquet file not found")
-          sys.exit(1)
-      except Exception as e:
-          print(e)
-          sys.exit(1)
-
+            start_time = time.time()
+            indexer = Indexer(args.output_path, create_mode=True)
+            indexer.index_from_dataframe(dataframe, fulltext_provider, split_term=";")
+            end_time = time.time()
+            console.log(f"[bold green] Finished after {end_time - start_time}")
+            sys.exit(os.EX_OK)
+        except FileNotFoundError:
+            console.log("[bold red] Input parquet file not found")
+            sys.exit(1)
+        except Exception as e:
+            print(e)
+            sys.exit(1)
 
 
 if __name__ == "__main__":
-    vm_env = lucene.getVMEnv() or lucene.initVM(vmargs=["-Djava.awt.headless=true"])
-    vm_env.attachCurrentThread()
     main()
